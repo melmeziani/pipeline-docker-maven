7ad5d43caac1ae7ed952ae1b7be099343056918e
Mohammed A. El Meziani <mohammed.elmeziani@keepler.io>
2019-12-26 16:46:20 +0100

    Update project path

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -4,5 +4,5 @@ echo "****************"
 echo "* Building jar!*"
 echo "****************"
 
-PROJ=/var/jenkins_home/workspace/pipeline-docker-maven
+PROJ=/Users/melmeziani/Projects/Formation/jenkins-course/jenkins_home/workspace/pipeline-docker-maven
 docker run --rm -v /root/.m2:/root/.m2 -v $PROJ/java-app:/app -w /app maven:3-alpine "$@"
--- jenkins/test/test.sh
+++ jenkins/test/test.sh
@@ -4,4 +4,4 @@ echo "################"
 echo "*** Testing  ***"
 echo "################"
 
-docker run --rm -v /root/.m2:/root/.m2 -v /var/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -w /app maven:3-alpine "$@"
+docker run --rm -v /root/.m2:/root/.m2 -v /Users/melmeziani/Projects/Formation/jenkins-course/jenkins_home/workspace/pipeline-docker-maven/java-app:/app -w /app maven:3-alpine "$@"
